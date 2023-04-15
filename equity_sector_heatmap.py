95bb438d7b45ba0fc0d9f2df22ba28ded2f5a8ab
Theo Deku <121975268+tdeku5@users.noreply.github.com>
2023-04-15 12:55:00 -0400

    Update equity_sector_heatmap.py

--- equity_sector_heatmap.py
+++ equity_sector_heatmap.py
@@ -3,6 +3,9 @@
 Created on Tue Apr 11 19:14:00 2023
 
 @author: tdeku
+
+This script queries equity sector ETF data from the SQL database created in yfinance and creates a 
+return correlation heatmap.
 """
 import sqlite3
 import pandas as pd
@@ -17,6 +20,8 @@ stocks = ['SPY', 'XLI', 'XLE', 'XLK', 'XLU', 'XLF', 'XLY', 'XLP', 'XLB', 'XLC', 
 
 dataframes = {}
 
+# Construct dataframe
+# 2018-07-01 is most recent date that data is available for ALL ETFs - XLC originated mid 2018
 for symbol in stocks:
     dataframes[symbol] = pd.read_sql_query(f"SELECT close from financial_data2 where symbol='{symbol}' and date>='2018-07-01'", con)
 
@@ -27,13 +32,9 @@ for symbol in stocks:
     df_stocks[symbol] = np.log(df_stocks[symbol]).diff()
     
 df_stocks = df_stocks.drop([0])
-#df_commodities.drop('PAL', axis=1, inplace=True)
 
+# Create heatmap
 plt.figure(figsize=(16,6))
 mask = np.triu(np.ones_like(df_stocks.corr(), dtype=np.bool))
 sector_map = sns.heatmap(df_stocks.corr(), mask=mask, vmin=-0.25, vmax=1, annot=True, cmap='BrBG')
 sector_map.set_title("Equity Sectors Correlation Heatmap")
-
-
-
-
