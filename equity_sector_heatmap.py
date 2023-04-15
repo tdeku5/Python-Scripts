b7e956ccee01d4c6a27b31495acd2682f92c2147
Theo Deku <121975268+tdeku5@users.noreply.github.com>
2023-04-15 12:50:57 -0400

    Add files via upload

---
+++ equity_sector_heatmap.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 11 19:14:00 2023
+
+@author: tdeku
+"""
+import sqlite3
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# connect to the database
+con = sqlite3.connect("financial_data2.db")
+
+stocks = ['SPY', 'XLI', 'XLE', 'XLK', 'XLU', 'XLF', 'XLY', 'XLP', 'XLB', 'XLC', 'XHB']
+
+dataframes = {}
+
+for symbol in stocks:
+    dataframes[symbol] = pd.read_sql_query(f"SELECT close from financial_data2 where symbol='{symbol}' and date>='2018-07-01'", con)
+
+df_stocks = pd.concat(dataframes, axis=1)
+df_stocks.columns = stocks
+
+for symbol in stocks:
+    df_stocks[symbol] = np.log(df_stocks[symbol]).diff()
+    
+df_stocks = df_stocks.drop([0])
+#df_commodities.drop('PAL', axis=1, inplace=True)
+
+plt.figure(figsize=(16,6))
+mask = np.triu(np.ones_like(df_stocks.corr(), dtype=np.bool))
+sector_map = sns.heatmap(df_stocks.corr(), mask=mask, vmin=-0.25, vmax=1, annot=True, cmap='BrBG')
+sector_map.set_title("Equity Sectors Correlation Heatmap")
+
+
+
+
